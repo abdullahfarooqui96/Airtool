cefd4a813f46301563177058cdfcb5da305b83e6
Muhammad Abdullah Farooqui <abdullah.farooqui.work@gmail.com>
2026-06-08 18:08:17 +0200

    setup workspace for report gen
    setup work space to add code for the report generator to create the reports for the program

--- report_generator.py
+++ report_generator.py
@@ -1,3 +1,23 @@
 # Airtool 
 # 62404051 | Muhammad Abdullah Farooqui
 # BOCP Final Project 
+"""
+report_generator.py - Report generation for AirTool.
+Generates traffic reports, trend analysis, and destination rankings.
+Uses nested loops where required by assignment specification.
+"""
+
+from collections import defaultdict
+
+from utils import (
+    print_header,
+    print_separator,
+    categorize_flights,
+    parse_month,
+    sort_records,
+    SORT_BY_PASSENGERS,
+    SORT_BY_AIRPORT,
+    SORT_BY_DESTINATION,
+    FLIGHT_DOMESTIC,
+    FLIGHT_INTERNATIONAL,
+)
